0225063627855cc2e6123ee453165be8eb72a428
Omari Blockton <oblockton@gmail.com>
2019-07-05 09:48:39 -0700

    edit db uri app.config

--- mars_flask.py
+++ mars_flask.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import mars_scrape
 
 app = Flask(__name__)
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_flask")
+app.config['MONGO_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+mongo = PyMongo(app)
 
 
 @app.route("/")
