882c4e96b14689285297ab77017e5d5d2e257924
Omari Blockton <oblockton@gmail.com>
2019-07-09 16:06:07 -0700

    refactor for selenium traversal

--- mars_scrape.py
+++ mars_scrape.py
@@ -9,22 +9,33 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 ## new deploy
-# 
-# options = Options()
-#
-# options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-#
-# options.add_argument('--headless')
-# options.add_argument('--disable-gpu')
-# options.add_argument('--no-sandbox')
-# browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
+
+options = Options()
+
+options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
 executable_path = {'executable_path':'GOOGLE_CHROME_BIN '}
 
 def scrape_mars():
     url = 'https://mars.nasa.gov/news/'
-    browser = Browser('chrome',**executable_path, headless=True) # <<<<<<----- Set headless=False to have browser pop-up
-    browser.visit(url)
-    html = browser.html
+
+    ########
+     '''for splinter traversal'''
+    ########
+    # # browser = Browser('chrome',**executable_path, headless=True) # <<<<<<----- Set headless=False to have browser pop-up
+    # browser.visit(url)
+    # html = browser.html
+
+    ##################
+     '''selenium traversal'''
+    ##################
+    browser.get(url)
+    html = browser.find_element_by_tag_name("body").get_attribute('innerHTML')
+
     # Create BeautifulSoup object; parse with 'lxml'
     nasa_gov = bs(html, 'lxml')
     # Returns a list of all the content title, only saving the first found (index [0]) as it is most recent.
@@ -36,11 +47,24 @@ def scrape_mars():
 
     # Set initial url to JPL mars topic content.
     url2 ="https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-    browser = Browser('chrome',**executable_path, headless=True) # <<<<<<----- Set headless=False to have browser pop-up
-    # Visit landing page
-    browser.visit(url2)
-    # Save HTML to variable
-    jpl_html = browser.html
+
+    #############
+     '''Splinter traversal'''
+    #############
+    # browser = Browser('chrome',**executable_path, headless=True) # <<<<<<----- Set headless=False to have browser pop-up
+    # # Visit landing page
+    # browser.visit(url2)
+    # # Save HTML to variable
+    # jpl_html = browser.html
+
+    ###################
+    '''Selenium  traversal'''
+    ###################
+    browser.get(url2)
+    jpl_html = browser.find_element_by_tag_name('body').get_attribute('innerHTML')
+
+
+
     # Create BSoup object. Parse HTML
     jpl = bs(jpl_html, 'html.parser')
     # Featured image on landing page is not the full size image.
@@ -49,11 +73,19 @@ def scrape_mars():
     jpl_base = 'https://www.jpl.nasa.gov'
     jpl_link_format = str( jpl_base + (jpl.find_all('footer')[0].a.get('data-link')))
 
-    #Visit the next page i.e the page where we find the full size image.
-    browser = Browser('chrome',**executable_path, headless=True) # <<<<<<----- Set headless=False to have browser pop-up
-    browser.visit(jpl_link_format)
-    # Save the html to var
-    jpl_target = browser.html
+    # #Visit the next page i.e the page where we find the full size image.
+    ######################## Splinter traversal
+    # browser = Browser('chrome',**executable_path, headless=True) # <<<<<<----- Set headless=False to have browser pop-up
+    # browser.visit(jpl_link_format)
+    # # Save the html to var
+    # jpl_target = browser.html
+
+    ###########################
+     '''Selenium traversal'''
+    ###########################
+    browser.get(jpl_link_format)
+    jpl_target = browser.find_element_by_tag_name('body').get_attribute('innerHTML')
+
     # Create new Soup object
     jpl_target_html = bs(jpl_target, 'html.parser')
     # Refactored: Find the featued image url. Concise method
@@ -63,11 +95,21 @@ def scrape_mars():
 
 
     mars_twit="https://twitter.com/marswxreport?lang=en"
-    browser = Browser('chrome',**executable_path, headless=True) # <<<<<<----- Set headless=False to have browser pop-up
-    #Visit the page
-    browser.visit(mars_twit)
-    # Save browsed page to var
-    mars_browse = browser.html
+    ####################
+     '''Splinter traversal'''
+     ######################
+    # browser = Browser('chrome',**executable_path, headless=True) # <<<<<<----- Set headless=False to have browser pop-up
+    # #Visit the page
+    # browser.visit(mars_twit)
+    # # Save browsed page to var
+    # mars_browse = browser.html
+
+    ###################
+     '''Selenium traversal'''
+    ##################
+    browser.get(mars_twit)
+    mars_browse = browser.find_element_by_tag_name('body').get_attribute('innerHTML')
+
     # Parse HTML
     mars_twit_html = bs(mars_browse,'html.parser')
     # Returns a list of all <P> tags wityh class 'tweet-text'. This is where the tweets written content is contained.
@@ -93,11 +135,22 @@ def scrape_mars():
 
     image_site_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     image_site_base = 'https://astrogeology.usgs.gov'
-    browser = Browser('chrome',**executable_path, headless=True) # <<<<<<----- Set headless=False to have browser pop-up
-    #Visit the page
-    browser.visit(image_site_url)
-    # Save browsed page to var
-    mars_img_browse = browser.html
+
+    ####################
+     '''Splinter traversal'''
+     ######################
+    # browser = Browser('chrome',**executable_path, headless=True) # <<<<<<----- Set headless=False to have browser pop-up
+    # #Visit the page
+    # browser.visit(image_site_url)
+    # # Save browsed page to var
+    # mars_img_browse = browser.html
+
+    ###################
+     '''Selenium traversal'''
+    ##################
+    browser.get(image_site_url)
+    mars_img_browse = browser.find_element_by_tag_name('body').get_attribute('innerHTML')
+
     # Parse HTML
     mars_img_html = bs(mars_img_browse,'html.parser')
     # Initialize list to store the link to the webpage for an individual hemisphere
@@ -113,11 +166,22 @@ def scrape_mars():
     #Loop through the 4 targets which correspond to individal pages for each hemisphere.
     # headless=True to execute without browser pop-up
     for target in mars_hemi_targets:
-        browser = Browser('chrome', headless=True)
-        #Visit the page
-        browser.visit(target)
-        # Save browsed page to var
-        target_obj = browser.html
+
+        ####################
+         '''Splinter traversal'''
+         ######################
+        # browser = Browser('chrome', headless=True)
+        # #Visit the page
+        # browser.visit(target)
+        # # Save browsed page to var
+        # target_obj = browser.html
+
+        ###################
+         '''Selenium traversal'''
+        ##################
+        browser.get(target)
+        target_obj = browser.find_element_by_tag_name('body').get_attribute('innerHTML')
+
         # Parse HTML
         target_html= bs(target_obj,'html.parser')
         # Even though the is only one h2 with class='title', find_all() returns the results as a list. Must use index [0].
@@ -128,8 +192,11 @@ def scrape_mars():
         hemi_title_url.append({'title':title,'img_url':hemi_url})
 
 
+
     content_data = {'news_title':news_title, 'news_content':news_content,
                    'featured_img_url':featured_img_url,'mars_tweet':mars_tweet,
                    'hemisphere_data':hemi_title_url, 'facts_tab_html': mars_facts_html}
+
+   browser.quit()
 
     return content_data
